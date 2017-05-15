c5d6d4850cf73a129479e173a16841d28c577bad
peterzhu08 <ZhuP0115@sduhsd.lan>
2017-05-15 09:26:54 -0700

    Changed User Interface and added more stuff to account for age and address on top of name

--- MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.zhup0115.mycontactapp;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
+
 /**
  * Created by zhup0115 on 5/12/2017.
  */
@@ -13,6 +15,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "AGE";
+    public static final String COL_4 = "ADDRESS";
 
     public DatabaseHelper (Context context){
         super (context, DATABASE_NAME, null, 1);
@@ -27,6 +31,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData(String name, String age, String address){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, age);
+        contentValues.put(COL_4, address);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1){
+            return false;
+        }
+        return true;
     }
 }
 
--- MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.zhup0115.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editAddress;
     Button btnAddData;
 
     @Override
@@ -18,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper (this);
+
+        editName = (EditText)(findViewById(R.id.editName));
+        editAge = (EditText)(findViewById(R.id.editAge));
+        editAddress = (EditText)(findViewById(R.id.editAddress));
+    }
+
+    public void addData (View v){
+        boolean isInserted = myDb.insertData(editName.getText().toString(),editAge.getText().toString(),editAddress.getText().toString());
+
+        if(isInserted == true){
+            Log.d("MyContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+            Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Log.d("MyContact", "Data insertion NOT successful");
+            //create toast message to user indicating data inserted incorrectly
+            Toast.makeText(getApplicationContext(), "NOT Success", Toast.LENGTH_SHORT).show();
+        }
     }
 }
