44382a77a2c16e36e1f28734d3b812eea6881556
peterzhu08 <ZhuP0115@sduhsd.lan>
2017-05-17 09:26:14 -0700

    finshed view data method still trying to debug
    view data button does not work and crashes when that button is clicked.

--- MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/zhup0115/mycontactapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zhup0115.mycontactapp;
 
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -50,24 +51,34 @@ public class MainActivity extends AppCompatActivity {
         Cursor res = myDb.getAllData();
         if (res.getCount() == 0){
             showMessage ("Error","No data found in database");
-            // put a Log.d message and toast
+            Log.d("MyContact", "No data found in database");
+            Toast.makeText(getApplicationContext(), "No data found in database", Toast.LENGTH_SHORT).show();
             return;
         }
         StringBuffer buffer = new StringBuffer();
         //setup loop with cursor movetonext method while loop
         // append each column(COL) to each buffer (use append method)
         // use get string method
-        while (res.getCount()>0){
 
-        }
+        res.moveToFirst();
+        while (res.moveToNext()){
+            for (int i = 1; i<=3 ; i++) {
+                buffer.append(res.getString(i));
+                buffer.append("/n");
+            }
 
-
-
+            buffer.append("\n");
+}
         showMessage ("Data", buffer.toString());
     }
 
 
 
-    private void showMessage(String error, String s) {
+    private void showMessage(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
